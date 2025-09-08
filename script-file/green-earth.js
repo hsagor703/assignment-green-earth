548c86df5b788f80c0ed77c7044665ce1880f32a
hsagor703 <hsagor703@gmail.com>
2025-09-08 12:07:35 +0600

    modale show complete

--- script-file/green-earth.js
+++ script-file/green-earth.js
@@ -1,6 +1,8 @@
 
 const categoriesContainer = document.getElementById("categories-container");
 const cardContainer = document.getElementById("card-container");
+const displayDetailsModal = document.getElementById("cardDetailsModal");
+const modalContainer = document.getElementById("modal-container")
 // all plant api 
 const displayAllPlant = () => {
   fetch("https://openapi.programming-hero.com/api/plants")
@@ -9,27 +11,17 @@ const displayAllPlant = () => {
       const plantData = data.plants;
       cardContainer.innerHTML=""
       plantData.forEach(pData => {
-        cardContainer.innerHTML += `
-             <div class="cards bg-white rounded-lg h-full overflow-hidden shadow-xl">
-                <img class="h-60 md:w-60 w-full  " src="${pData.image}" alt="">
-                <h2 class="font-semibold p-3">${pData.name}</h2>
-                <p class="text-sm my-2 p-3">${pData.description}</p>
-                <div class="flex justify-between items-center p-3">
-                  <p class="bg-green-300 text-green-800 px-2 rounded-lg font-medium">${pData.category}</p>
-                  <p class="font-semibold">৳${pData.price}</p>
-                </div>
-                <div class="p-3"><button class="p-1.5 mt-2  w-full rounded-full text-white bg-green-700">Add to Cart</button></div>
-              </div>
-        `;
-      });
-      
+        displayCardContainer(pData)
+      }).catch((err) => {
+      console.log(err);
+    });
     });
 }
 
 
 // categories api
 const categories = () => {
-  displayAllPlant()
+  displayAllPlant();
   fetch("https://openapi.programming-hero.com/api/categories")
     .then((res) => res.json())
     .then((data) => {
@@ -40,36 +32,44 @@ const categories = () => {
         const categoriesItemsNamesId = name.id;
         categoriesContainer.innerHTML += `
             <h1 id="${categoriesItemsNamesId}" class=" p-2 hover:bg-green-700 rounded-lg font-medium hover:text-white">${categoriesItemsNames}</h1>
-            `;
-            
+            `;            
       });
+    }).catch((err) => {
+      console.log(err);
     });
 };
 
 // categories and card api
 const categoriesAndCard = (id) => {
-        console.log(id);
   fetch(`https://openapi.programming-hero.com/api/category/${id}`)
     .then((res) => res.json())
     .then((data) => {
       const plantData = data.plants;
       cardContainer.innerHTML=""
       plantData.forEach(pData => {
-        cardContainer.innerHTML += `
+        console.log(pData);
+        displayCardContainer(pData);
+      });
+      
+    }).catch((err) => {
+      console.log(err);
+    });
+};
+
+const displayCardContainer = (pData) => {
+cardContainer.innerHTML += `
              <div class="cards bg-white rounded-lg h-full overflow-hidden shadow-xl">
                 <img class="h-60 md:w-60 w-full  " src="${pData.image}" alt="">
-                <h2 class="font-semibold p-3">${pData.name}</h2>
+                <h2 id
+                ="${pData.id}" class="card-title font-semibold p-3">${pData.name}</h2>
                 <p class="text-sm my-2 p-3">${pData.description}</p>
                 <div class="flex justify-between items-center p-3">
                   <p class="bg-green-300 text-green-800 px-2 rounded-lg font-medium">${pData.category}</p>
                   <p class="font-semibold">৳${pData.price}</p>
                 </div>
-                <div class="p-3"><button class="p-1.5 mt-2  w-full rounded-full text-white bg-green-700">Add to Cart</button></div>
+                <div class="p-3"><button class="card-btn p-1.5 mt-2  w-full rounded-full text-white bg-green-700">Add to Cart</button></div>
               </div>
         `;
-      });
-      
-    });
 };
 
 // click categories items too show categories data
@@ -78,5 +78,32 @@ categoriesContainer.addEventListener("click", (e) => {
   // if (e.target.innerText === ) {
 
   // }
+});
+
+// all plant details api 
+cardContainer.addEventListener("click", (e) => {
+  console.log(e.target);
+  if (e.target.classList.contains("card-title")) {
+    const id = e.target.id;
+     fetch(`https://openapi.programming-hero.com/api/plant/${id}`)
+      .then(res => res.json())
+      .then(data => {
+    modalContainer.innerHTML = `
+     <div class="">
+       <h2 id="" class="font-bold ">${data.plants.name}</h2>
+      <img class="h-60 w-full my-3 rounded-lg" src="${data.plants.image}" alt="">
+      <p class="rounded-lg mb-2"><span class="font-bold">Category</span>: ${data.plants.category}</p>
+      <p class=""><span class="font-bold">Price</span>: ৳${data.plants.price}</p>
+      <p class="text-sm my-2 "><span class="font-bold">Description</span>: ${data.plants.description}</p>
+     </div>
+    `
+  });
+  displayDetailsModal.showModal();
+};
+
+if (e.target.classList.contains("card-btn")) {
+  const title = e.target.innerText
+  console.log("click successfully", title);
+}
 });
 categories();
